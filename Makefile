8581140748ea213b8c6491716800669a4827c837
sid <s208@imperial.ac.uk>
2017-09-15 18:52:00 +0800

    updated tutorial-01 to sdl2 and also makefile to automatically detect macOS or Linux

--- Makefile
+++ Makefile
@@ -1,14 +1,23 @@
-LIB_FLAGS			:= -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+	LIB_FLAGS				:= -lSDL2
+	INCLUDE_FLAGS			:= 
+endif
+ifeq ($(UNAME_S),Darwin)
+	INCLUDE_FLAGS			:= -I/opt/local/include/SDL2 -I/opt/local/include -D_THREAD_SAFE 
+	LIB_FLAGS				:= -L/opt/local/lib -lSDL2
+endif
+
 
 all: tutorial-01 tutorial-02
 
 tutorial-01: tutorial_01/main.cpp tutorial_01/hello.bmp
 	@mkdir -p bin
-	@g++ -o./bin/$@ tutorial_01/main.cpp $(LIB_FLAGS)
+	@g++ -o./bin/$@ $(INCLUDE_FLAGS) tutorial_01/main.cpp $(LIB_FLAGS)
 
 tutorial-02: tutorial_02/main.cpp tutorial_02/hello.bmp
 	@mkdir -p bin
-	@g++ -o./bin/$@ tutorial_02/main.cpp $(LIB_FLAGS)
+	@g++ -o./bin/$@ $(INCLUDE_FLAGS) tutorial_02/main.cpp $(LIB_FLAGS)
 
 clean:
 	@rm -rf bin
--- tutorial_01/main.cpp
+++ tutorial_01/main.cpp
@@ -1,36 +1,86 @@
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
-#include "SDL/SDL_ttf.h"
-#include "SDL/SDL_mixer.h"
+#include <SDL2/SDL.h>
+
+int SCREEN_WIDTH = 640;
+int SCREEN_HEIGHT = 480;
+
+SDL_Window *gWindow = NULL;				// This is the window to render on to
+SDL_Surface* gScreenSurface = NULL; 	// Surface contained by the window
+SDL_Surface* gHelloWorld = NULL; 		// Image to load and show on the screen
+
+// initialization function
+bool init() {
+	// Initialization flag
+	bool success = true;
+
+	// Initialize SDL
+	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	} else {
+		// Create Window
+		gWindow = SDL_CreateWindow( "SDL_Tutorial_01", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if ( gWindow == NULL ) {
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		} else {
+			// Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia() {
+	// Success flag
+	bool success = true;
+
+	// Load splash image
+	gHelloWorld = SDL_LoadBMP( "tutorial_01/hello.bmp" );
+	if ( gHelloWorld == NULL ) {
+		printf( "Unable to load image %s! SDL Error: %s\n", "tutorial_01/hello.bmp", SDL_GetError() );
+		success = false;
+	}
+
+	return success;
+}
+
+void close() {
+	// Free surface
+	SDL_FreeSurface( gHelloWorld );
+	gHelloWorld = NULL;
+
+	// Destroy Window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	// Quit SDL Subsystem
+	SDL_Quit();
+}
 
 int main(int argc, char *args[]) {
-    // The images
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
     
-    // Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
+	// Initialize
+	if ( !init() ) {
+		printf( "Failed to initialize!\n" );
+	} else {
+		// Load media
+		if ( !loadMedia() ) {
+			printf( "Failed to load media!\n" );
+		} else {
+			// Apply the image
+			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+			
+			// Update the surface
+			SDL_UpdateWindowSurface( gWindow );
 
-    // Set up screen
-    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+			// Wait 2 seconds
+			SDL_Delay( 2000 );
+		}
+	}
 
-    // Load image
-    hello = SDL_LoadBMP( "tutorial_01/hello.bmp" );
+	// Free and quit SDL
+	close();
 
-    // Apply image to screen
-    SDL_BlitSurface( hello, NULL, screen, NULL );
-
-    // Update Screen
-    SDL_Flip( screen );
-
-    // Pause
-    SDL_Delay( 2000 );
-
-    // Free the loaded image
-    SDL_FreeSurface( hello );
-
-    // Quit SDL
-    SDL_Quit();
-
-    return 0;
+	return 0;
 }
